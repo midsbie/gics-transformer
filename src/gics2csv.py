e90b99c9aacc8e17b21d94c8f0f7db4900baebee
Miguel Guedes <miguel@softgeist.com>
2023-08-08 16:18:02 +0100

    gics2csv: add support for dropping discontinued classifications

--- src/gics2csv.py
+++ src/gics2csv.py
@@ -1,19 +1,50 @@
-import pandas as pd
 import argparse
+import re
+
+import pandas as pd
+from openpyxl import load_workbook
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description="""Process an XLSX file that
-    contains official Global Industry Classification Standard (GICS) released by
-    MSCI, transforming and normalizing the data, and outputting the results as a CSV
-    file.""")
+    contains the official Global Industry Classification Standard (GICS)
+    released by MSCI, transforming and normalizing the data, and outputting the
+    results as a CSV file.""")
     parser.add_argument("filename", type=str,
                         help="The path to the XLSX file to process.")
     parser.add_argument("--sheet", type=str, default=0,
                         help="Sheet in the XLSX to use. Default: first sheet.")
     parser.add_argument("--output", type=str, default="output.csv",
                         help="Output CSV file name. Default: output.csv.")
+    parser.add_argument("--drop-discontinued", type=bool, default=False,
+                        help="Remove discontinued classifications. Default: False.")
     return parser
+
+
+def get_codes_to_remove(filename, sheet):
+    wb = load_workbook(filename=filename, data_only=True)
+    ws = wb[sheet]
+
+    pattern = r'\(.*?\bdiscontinued\b.*?\)'
+    codes = []
+
+    # The code for a classification is added to the `code` array if a cell is
+    # found to contain the strikethrough formatting or the word "discontinued"
+    # is found enclosed within parentheses in a cell.
+    for row in ws.iter_rows():
+        for cell in row:
+            if not cell.font.strikethrough and not re.search(
+                    pattern, str(cell.value), re.IGNORECASE):
+                continue
+
+            code = row[6].value
+            if code is not None:
+                codes.append(code)
+
+            break
+
+    wb.close()
+    return codes
 
 
 def normalize_merged_cols(cols, subject, code_col, name_col):
@@ -23,7 +54,10 @@ def normalize_merged_cols(cols, subject, code_col, name_col):
 
 
 def main(args):
-    # Load the data from the specified file and sheet
+    # Get a list of codes to remove (done further down below), if applicable.
+    codes_to_remove = get_codes_to_remove(
+        args.filename, args.sheet) if args.drop_discontinued else []
+
     df = pd.read_excel(args.filename, sheet_name=args.sheet, header=None)
 
     # Identify header row
@@ -53,8 +87,12 @@ def main(args):
         if i < len(df) - 1:
             df.at[i, 'industry_description'] = df.at[i + 1, 'sub_industry_name']
 
-    # Remove every second row containing the descriptions
+    # Remove every second row containing the descriptions that we've just copied
     df = df.iloc[::2].reset_index(drop=True)
+
+    # Remove rows that refer to classifications that have been discontinued
+    if args.drop_discontinued and len(codes_to_remove) > 0:
+        df = df[~df['sub_industry_code'].isin(codes_to_remove)]
 
     # Fix whitespace before filling NA
     df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
@@ -77,9 +115,10 @@ def main(args):
     # Fixing more oddities:
     # - Collapse multiple spaces into one
     df.replace(r'\s\s+', ' ', regex=True, inplace=True)
-    # - In-column annotations
+    # - Remove in-column annotations
     df.replace(r'\([^)]*\)', '', regex=True, inplace=True)
 
+    # Done!  Persist to CSV file.
     df.to_csv(args.output, index=False)
 
 
